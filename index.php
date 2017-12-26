34e211829eaabd4ac1507b53241b9055851dc2d3
huangjianquan <huangjianquan@modian.com>
2017-12-26 22:18:24 +0800

    zhima

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $userAuthUrl = "https://openauth.alipay.com/oauth2/publicAppAuthorize.htm?";
 $data['app_id']       =  '2017122201059023';
 $data['scope']        =  'auth_base';
-$data['redirect_uri'] =  'http://zhima.ingdu.cn/credit/creditScoreNotify';
+$data['redirect_uri'] =  'https://www.baidu.com';
 $data['state']        =  '345';
 $url = $userAuthUrl.http_build_query($data);
 header("Location:".'alipays://platformapi/startapp?appId=20000067&url='.$url);
