41eb2588d8b3b5d5d877cb9cd810fe2f4e82ed3b
huangjianquan <huangjianquan@modian.com>
2017-12-27 00:19:58 +0800

    ali

--- auth.php
+++ auth.php
@@ -38,16 +38,18 @@ $access_token = $result['alipay_system_oauth_token_response']['access_token'];
 
 
 $aopObj->auth_token            = $access_token;
+echo access_token;
 require_once('aop/request/ZhimaCreditScoreGetRequest.php');
 
 $request = new ZhimaCreditScoreGetRequest ();
 //        var_dump($request);exit;
-$data['transaction_id'] = "201512100936588040000000465159";
+$data['transaction_id'] = "201512100936588040000000465144";
 $data['product_code']   = "w1010100100000000001";
 
 $request->setBizContent(json_encode($data));
 
 $result = $aopObj->execute ( $request  );
+var_dump($result);exit;
 
 
 $responseNode = str_replace(".", "_", $request->getApiMethodName()) . "_response";
