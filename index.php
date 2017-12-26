f06c191661887ef3d5c0edd9fe32f62e1c689a26
huangjianquan <huangjianquan@modian.com>
2017-12-26 22:10:38 +0800

    zhima

--- index.php
+++ index.php
@@ -6,5 +6,5 @@ $data['scope']        =  'auth_base';
 $data['redirect_uri'] =  'http://zhima.ingdu.cn/credit/creditScoreNotify';
 $data['state']        =  '345';
 $url = $userAuthUrl.http_build_query($data);
-header("Location:".'alipays://platformapi/startapp?appId=20000067&url='$url);
+header("Location:".'alipays://platformapi/startapp?appId=20000067&url='.$url);
     
