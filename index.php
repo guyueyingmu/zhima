42115fe21bc9f74091a034bd8489100702e0e4b4
huangjianquan <huangjianquan@modian.com>
2017-12-27 00:25:01 +0800

    ali

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 $userAuthUrl = "https://openauth.alipay.com/oauth2/publicAppAuthorize.htm?";
 $data['app_id']       =  '2017122201059023';
-$data['scope']        =  'auth_base';
+$data['scope']        =  'auth_user';
 $data['redirect_uri'] =  'http://zhima.ingdu.cn/auth.php';
 $data['state']        =  '345';
 $url = $userAuthUrl.http_build_query($data);
